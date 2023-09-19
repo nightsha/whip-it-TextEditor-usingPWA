ca5660f9b3822c513c452463fe662953084da9d1
nightsha <130249622+nightsha@users.noreply.github.com>
2023-09-19 14:10:18 -0500

    codedDatabaseDid TodosRefactored

---
+++ client/src/js/database.js
@@ -0,0 +1,70 @@
+// import { request } from 'express';
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+// code copied from chat.openai
+export const putDb = async (content) => {
+  console.log('put to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value:content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result.value);
+  
+};
+
+
+  
+
+
+
+console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+// code copied from chat.openai
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result.value);
+  return result?.value;
+};
+ console.error('getDb not implemented');
+
+initdb();
